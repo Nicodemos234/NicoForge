5e9927bb2e6c249d9d6c65a7ec5de12df475e03f
Nicodemos234 <nicodemos54324@gmail.com>
2026-01-20 17:22:01 -0300

    Add redirects

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,30 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  async redirects() {
+    return [
+      {
+        source: "/twitch",
+        destination: "https://twitch.tv/NicoForge",
+        permanent: true,
+      },
+      {
+        source: "/youtube",
+        destination: "https://youtube.com/@Nico_Forge",
+        permanent: true,
+      },
+      {
+        source: "/github",
+        destination: "https://github.com/Nicodemos234",
+        permanent: true,
+      },
+      {
+        source: "/instagram",
+        destination: "https://instagram.com/nico_forge",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
